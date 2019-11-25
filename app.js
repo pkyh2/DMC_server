e52d61d47e4b072d037b1376aab2203852b8320b
Kimgahyeon <kimgahyun1@ajou.ac.kr>
2019-11-25 16:55:14 +0900

    DB 없이 배포하는 버전

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
-//   console.log('The solution is: ', rows[0].solution);
-// });
-
-// connection.end();
-
-
 // routers
 var indexRouter = require('./routes/index');
 var projectRouter = require('./routes/project');
@@ -20,9 +12,13 @@ var contactRouter = require('./routes/contact');
 var teamRouter = require('./routes/team');
 var paperRouter = require('./routes/papers');
 var blockchainRouter = require('./routes/blockchain');
-var adminRouter = require('./routes/admin');
+// var adminRouter = require('./routes/admin');
 var blueprintRouter = require('./routes/blueprint');
 var teamateRouter = require('./routes/teamate');
+var healthRouter = require('./routes/health');
+var networkRouter = require('./routes/network');
+var etherappRouter = require('./routes/etherapp');
+var energyRouter = require('./routes/energy');
 
 
 var app = express();
@@ -45,7 +41,12 @@ app.use('/papers', paperRouter);
 app.use('/blueprint', blueprintRouter);
 app.use('/teamate', teamateRouter);
 app.use('/blockchain', blockchainRouter);
-app.use('/admin', adminRouter);
+app.use('/health', healthRouter);
+app.use('/network', networkRouter);
+app.use('/etherapp', etherappRouter);
+app.use('/energy', energyRouter);
+
+// app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/papers.js
+++ routes/papers.js
@@ -3,33 +3,44 @@ var router = express.Router();
 var fs = require('fs');
 
 // db setup
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rlarkgus',
-  database: 'dmc',
-  multipleStatements: true
-});
-connection.connect();
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'rlarkgus',
+//   database: 'dmc',
+//   multipleStatements: true
+// });
+// connection.connect();
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  var indexQuery = 'SELECT * FROM papers';
-  var indexAllPapers = connection.query(indexQuery, function (err, rows) {
-    if (err) throw err;
-    else {
-      for(i in rows){
-        console.log(rows[i]._id);
-        console.log(rows[i].title);
-        console.log(rows[i].author);
-        console.log(rows[i].info);
-        console.log("--------------");
-      }
-    }
-    res.render('papers', { title: 'Distributed Mobile Computing', rows: rows});
-  });
+  var jsonFile = fs.readFileSync('./public/files/papers.json')
+
+  var obj = JSON.parse(jsonFile);
+  var obj_length = Object.keys(obj.papers).length;
+
+  console.log(obj.papers[0]);
+  console.log(obj.papers[1]);
+  console.log(obj_length)
+
+  res.render('papers', { title: 'Distributed Mobile Computing', papers: obj, obj_length: obj_length});
+
+  // var indexQuery = 'SELECT * FROM papers';
+  // var indexAllPapers = connection.query(indexQuery, function (err, rows) {
+  //   if (err) throw err;
+  //   else {
+  //     for(i in rows){
+  //       console.log(rows[i]._id);
+  //       console.log(rows[i].title);
+  //       console.log(rows[i].author);
+  //       console.log(rows[i].info);
+  //       console.log("--------------");
+  //     }
+  //   }
+  //   res.render('papers', { title: 'Distributed Mobile Computing', rows: rows});
+  // });
 });
 module.exports = router;
 
